7275d04d902f7f429fd54b14078fca847dae362f
geoorgeqq <grajdeanu.george@yahoo.ro>
2026-01-20 07:58:25 +0200

    fix 3

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/terminal-portofolio/",
+  base: "https://geoorgeqq.github.io/terminal-portofolio/",
 });
